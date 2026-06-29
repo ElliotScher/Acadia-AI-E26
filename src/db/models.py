0972cf5bc1a07f3335aa639522f5ff9dd7f34ce1
kyleplo <31634240+kyleplo@users.noreply.github.com>
2026-06-29 11:15:08 -0400

    Database schemas and tests

---
+++ src/db/models.py
@@ -0,0 +1,108 @@
+from sqlalchemy import String, DateTime, Float, Integer, Boolean, ForeignKey, DDL, select, desc, event
+from sqlalchemy.orm import Mapped, WriteOnlyMapped, Session, DeclarativeBase, mapped_column, relationship
+from datetime import datetime, timedelta
+
+class Base(DeclarativeBase):
+    pass
+
+class Image(Base):
+    __tablename__ = "image"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    path: Mapped[str] = mapped_column(String(), unique=True, nullable=False)
+    datetime: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
+
+    instances: WriteOnlyMapped['Instance'] = relationship(back_populates='image', cascade='all, delete-orphan', single_parent=True, passive_deletes=True)
+
+    def get_instances (self, session: Session) -> list['Instance']:
+        return session.scalars(self.instances.select()).all()
+    
+    def get_entities (self, session: Session) -> list['Instance']:
+        return session.scalars(select(Entity).join(Instance).where(Instance.image_id == self.id)).all()
+
+    def __repr__ (self) -> str:
+        return f"Image({self.id})"
+
+class Entity(Base):
+    __tablename__ = "entity"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    speed: Mapped[float] = mapped_column(Float(), nullable=True)
+    direction: Mapped[int] = mapped_column(Integer(), nullable=True)
+    ebike: Mapped[bool] = mapped_column(Boolean(), nullable=True)
+    cluster: Mapped[int] = mapped_column(Integer(), nullable=True)
+
+    instances: WriteOnlyMapped['Instance'] = relationship(back_populates='entity', cascade='all, delete-orphan', single_parent=True, passive_deletes=True)
+
+    def get_instances (self, session: Session) -> list['Instance']:
+        return session.scalars(self.instances.select()).all()
+    
+    def get_type_id (self, session: Session) -> int:
+        return session.scalars(self.instances.select().limit(1)).one().type_id
+    
+    def get_earliest_image(self, session: Session) -> Image:
+        return session.scalars(select(Image).join(Instance).where(Instance.entity_id == self.id).order_by(Image.datetime).limit(1)).one()
+    
+    def get_latest_image(self, session: Session) -> Image:
+        return session.scalars(select(Image).join(Instance).where(Instance.entity_id == self.id).order_by(desc(Image.datetime)).limit(1)).one()
+
+    def get_timedelta(self, session: Session) -> timedelta:
+        images: List[Image] = session.scalars(select(Image).join(Instance).where(Instance.entity_id == self.id).order_by(desc(Image.datetime))).all()
+        if len(images) < 2:
+            return timedelta()
+        return images[0].datetime - images[-1].datetime
+    
+    def get_entities_in_cluster(self, session: Session) -> list['Entity']:
+        return session.scalars(select(Entity).where(Entity.cluster == self.cluster)).all()
+    
+    def __repr__ (self) -> str:
+        return f"Entity({self.id})"
+
+class Instance(Base):
+    __tablename__ = "instance"
+    image_id: Mapped[int] = mapped_column(ForeignKey(Image.id, ondelete="cascade", onupdate="restrict"), primary_key=True)
+    entity_id: Mapped[int] = mapped_column(ForeignKey(Entity.id, ondelete="cascade", onupdate="no action"), primary_key=True)
+    type_id: Mapped[int] = mapped_column(Integer(), nullable=False)
+    x: Mapped[int] = mapped_column(Integer(), nullable=False)
+    y: Mapped[int] = mapped_column(Integer(), nullable=False)
+    width: Mapped[int] = mapped_column(Integer(), nullable=False)
+    height: Mapped[int] = mapped_column(Integer(), nullable=False)
+
+    image: Mapped[Image] = relationship(back_populates='instances')
+    entity: Mapped[Entity] = relationship(back_populates='instances')
+
+    def __repr__ (self) -> str:
+        return f"Instance({self.image_id}, {self.entity_id})"
+
+trigger_ddl = DDL("""
+CREATE TRIGGER delete_entity_when_last_instance_deleted
+AFTER DELETE ON instance
+FOR EACH ROW
+WHEN NOT EXISTS (
+  SELECT 1 FROM instance WHERE entity_id = OLD.entity_id
+)
+BEGIN
+  DELETE FROM entity WHERE id = OLD.entity_id;
+END;
+""")
+
+trigger2_ddl = DDL("""
+CREATE TRIGGER delete_entity_when_last_instance_updated
+AFTER UPDATE OF entity_id ON instance
+FOR EACH ROW
+WHEN NOT EXISTS (
+  SELECT 1 FROM instance WHERE entity_id = OLD.entity_id
+)
+BEGIN
+  DELETE FROM entity WHERE id = OLD.entity_id;
+END;
+""")
+
+event.listen(
+    Base.metadata,
+    "after_create",
+    trigger_ddl.execute_if(dialect="sqlite"),
+)
+event.listen(
+    Base.metadata,
+    "after_create",
+    trigger2_ddl.execute_if(dialect="sqlite"),
+)
