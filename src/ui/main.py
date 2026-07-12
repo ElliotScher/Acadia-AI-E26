1c5c794217e4b337f6da0d3333fb937882786226
kyleplo <31634240+kyleplo@users.noreply.github.com>
2026-07-11 21:02:29 -0400

    feat(ui): ui for bike/rider merging

--- src/ui/main.py
+++ src/ui/main.py
@@ -13,6 +13,7 @@ import utility.parallel as upl
 from db import get_db
 from db.models import Image
 
+
 class Root(QtWidgets.QMainWindow):
     db: Engine
     session: Session
@@ -64,12 +65,12 @@ class Root(QtWidgets.QMainWindow):
         aAnalyzeAll = QtGui.QAction("Analyze All", self)
         aAnalyzeAll.triggered.connect(self.analyzeAll)
         mAnalyze.addAction(aAnalyzeAll)
-        aMergeBikes = QtGui.QAction("Merge Bikes and Riders", self)
-        aMergeBikes.triggered.connect(self.analyzeMergeBikes)
-        mAnalyze.addAction(aMergeBikes)
-        aAnalyzeAll = QtGui.QAction("Analyze All", self)
-        aAnalyzeAll.triggered.connect(self.analyzeAll)
-        mAnalyze.addAction(aAnalyzeAll)
+        aMergeBikesFiltered = QtGui.QAction("Merge Filtered Bikes and Riders", self)
+        aMergeBikesFiltered.triggered.connect(self.analyzeMergeBikesFiltered)
+        mAnalyze.addAction(aMergeBikesFiltered)
+        aMergeBikesAll = QtGui.QAction("Merge All Bikes and Riders", self)
+        aMergeBikesAll.triggered.connect(self.analyzeMergeBikesAll)
+        mAnalyze.addAction(aMergeBikesAll)
 
         mSelect = self.menuBar().addMenu("Select")
         aSelectAll = QtGui.QAction("Select All", self)
@@ -141,11 +142,14 @@ class Root(QtWidgets.QMainWindow):
     def analyzeAll(self):
         if self.tabs.currentWidget() == self.imageTab:
             self.imageTab.analyze(False)
-    
+
     @QtCore.Slot()
-    def analyzeMergeBikes(self):
-        if hasattr(self, "session"):
-            merge_bikes_riders(self.session, 0.2)
+    def analyzeMergeBikesFiltered(self):
+        self.imageTab.mergeBikes(True)
+
+    @QtCore.Slot()
+    def analyzeMergeBikesAll(self):
+        self.imageTab.mergeBikes(False)
 
 
 class EntitiesTab(QtWidgets.QWidget):
