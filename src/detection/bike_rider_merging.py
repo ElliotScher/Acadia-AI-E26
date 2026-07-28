b06cdcaabf36a0dff0829bc6d05012173db20d45
simirian <simirian5813@gmail.com>
2026-07-28 12:53:31 -0400

    feat(detect): merge bikes and riders positionally
    Adds an alternative merging method which merges bikes and riders based
    on their position rather than their overlap, still a little buggy
    somehow.

--- src/detection/bike_rider_merging.py
+++ src/detection/bike_rider_merging.py
@@ -1,3 +1,4 @@
+import math
 from sqlalchemy.orm import Session
 from db.models import Image, Instance
 
@@ -10,8 +11,15 @@ def merge_bikes_riders(session: Session, image: Image, threshold: float):
     people: filter[Instance] = filter(lambda i: i.type_id == 0, instances)
 
     for bike in bikes:
+        btx = bike.x + bike.width / 2
+        bty = bike.y
         for person in people:
-            if person not in session.deleted and bike.overlap_with(person) >= threshold:
+            ptx = person.x + person.width / 2
+            pty = person.y + person.height / 2
+            dist = math.sqrt((btx - ptx) ** 2 + (bty - pty) ** 2)
+            if person not in session.deleted and (
+                dist < person.width / 2 or bike.overlap_with(person) >= threshold
+            ):
                 x1 = min(bike.x, person.x)
                 y1 = min(bike.y, person.y)
                 x2 = max(bike.x + bike.width, person.x + person.width)
